c84ee6a73d7b353d48e6b3c377f2edc89db67e59
January Johnson <jpjansa@gmail.com>
2020-06-25 17:39:27 -0500

    initialized respository

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by CompSci 4220 Student on 6/25/20.
+//  Copyright © 2020 CompSci 4220 JanuaryJohnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
